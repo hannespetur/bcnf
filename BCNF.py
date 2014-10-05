d7efee500f74d00f5504266912821f06ad6d27ed
Hannes Pétur Eggertsson <hannespetur@gmail.com>
2014-10-05 04:46:26 +0000

    More functions and translation
    Added more functionality (closure, keys, superkeys and a different way
    to write FDs) and the print statements were translated to Icelandic.

--- BCNF.py
+++ BCNF.py
@@ -1,17 +1,21 @@
-# author Xuefeng Zhu 
+﻿from sets import Set
+import itertools
 
-from sets import Set
+## Class fyrir fallákveður (e. functional dependencies)
 class FD:
+	## Fallákveður hefur mengi A og B sem eru ekki tóm þar sem A->B
 	def __init__(self, A, B):
 		self.A = Set(A)	
 		self.B = Set(B)
 		self.total = Set(A+B)
 
+	## Lokun (e. closure): Táknað A+. Tekur inn mengi af A og fallákveður og ákvarðar lokunina.
 	def closure(self, fd):
 		if (fd.A.issubset(self.total) and not fd.B.issubset(self.total)):
 			self.total = self.total.union(fd.B)
 			return True
 
+## Class fyrir töflur
 class Table:
 	def __init__(self, Attrs, FDs):
 		self.Attrs = Attrs
@@ -42,6 +46,21 @@ class Table:
 			if (FD.A.issubset(attrs) and FD.B.issubset(attrs)):
 				result.append(FD)
 		return result
+
+	def stringsClosureOneRHSAndKeys(self):
+		closure = ""
+		allFDsWithOneRHS = ""
+		lyklar = ""
+		listofkeys = [list(self.Attrs)]
+		
+		for i in xrange(len(self.Attrs)):
+			eigindi = "".join(list(self.Attrs))
+			for j in itertools.combinations(eigindi,i+1):
+				fds = findClosure(self,j)
+				closure += formFDs(fds)+"\n"
+				allFDsWithOneRHS += formFDOneRHS(fds)
+				lyklar,listofkeys = findKeys(self,listofkeys,lyklar,fds,j)	
+		return closure, allFDsWithOneRHS, lyklar
 		
 def getFDs(fds):
 	result = []
@@ -57,51 +76,100 @@ def formFDs(fds):
 		if (i != len(fds)-1):
 			result += ", "
 	return result
+	
+def findClosure(self,attributes):
+	fds = "".join(attributes)+"->"+"".join(attributes)
+	fds = fds.split(",")
+	fds = getFDs(fds)
+	k = 0
+	while k < len(self.FDs):
+		FD = self.FDs[k]
+		if (FD.A.issubset(fds[0].total) and not FD.B.issubset(fds[0].B)):
+			fds[0].total = FD.B.union(fds[0].total)
+			fds[0].B = FD.B.union(fds[0].B)
+			k = 0
+		else:
+			k += 1
+	return fds
+
+def findKeys(self,listofkeys,lyklar,fds,attributes):
+	index = 0
+	while index < len(listofkeys):
+		key = listofkeys[index]
+		if fds[0].B.issuperset(self.Attrs):
+			if fds[0].A.issuperset(Set(key)):
+				lyklar += "".join(list(fds[0].A))+" er yfirlykill\n"
+				break
+		index += 1
+		if index == len(listofkeys) and fds[0].B.issuperset(self.Attrs):
+			listofkeys.append(fds[0].A)
+			lyklar += "".join(list(fds[0].A))+" er lykill (og yfirlykill)\n"
+			index += 1
+	return lyklar, listofkeys
+
+def formFDOneRHS(fds):
+	## Þetta fall tekur inn fallákveður (FDs) og skilar streng af öllum ófáfengilegum fallákveðum með einn eiginleika hægra megin.
+	result = ""
+	for i in range(len(fds)):
+		base = "".join(list(fds[i].A))
+		fdsBlist = list(fds[i].B)
+		for j in range(len(fdsBlist)):
+			# Tökum í burtu allar fáfengnar fallákveður
+			if not Set(fdsBlist[j]).issubset(fds[i].A):
+				result += base+"->"+fdsBlist[j]+"\n"
+	return result
 
 if __name__ == "__main__":
-	attrs = raw_input("Please enter table attributes, seperated by comma\n")
+	attrs = raw_input("Vinsamlegast sláið inn öll eigindi með kommu á milli t.d. A,B,C:\n")
 	attrs = Set(attrs.split(','))
-	fds = raw_input("Please enter function dependencies, like AB->C, seperated by comma\n")
+	fds = raw_input("Vinsamlegast sláið inn allar fallákveður með kommu á milli t.d. AB->C,C->D:\n")
 	fds = fds.split(',')
 	fds = getFDs(fds)
 	tables = [Table(attrs, fds)]
-
-	print 
-	print "USER INPUT"
-	print "Attributes:", ",".join(list(attrs))
-	print "FDs:", formFDs(fds)
-
+	closure, allFDsWithOneRHS, lyklar = tables[0].stringsClosureOneRHSAndKeys();
+	
+	print "----------------------------------------------------------------"
+	print "-- USER INPUT --"
+	print "Eigindi:", ",".join(list(attrs))
+	print "Fallákveður:", formFDs(fds)
+	print "----------------------------------------------------------------"
+	print "-- LOKANIR --"
+	print "Lokanir (e. closure) allra samsetninga af eigindum eru:"
+	print closure
+	print "----------------------------------------------------------------"
+	print "-- EINN EIGINLEIKI HÆGRA MEGIN --"
+	print "Allar ófáfengilegum fallákveður með einn eiginleika hægra megin:"
+	print allFDsWithOneRHS
+	print "----------------------------------------------------------------"
+	print "-- LYKLAR --"
+	print "Allir lyklar og yfirlyklar eru:"
+	print lyklar,
+	print "----------------------------------------------------------------"
+	print "-- BCNF --"
+	
 	illDF = tables[0].findIllDF();
 	if (illDF == 0):
-		print "The given Table and its Functional Dependencies is in BCNF"
+		print "Taflan er á BCNF formi!"
 	else:
-		print "The given Table and its Functional Dependencies is not in BCNF"
+		print "Taflan er EKKI á BCNF formi!"
 		print 
-		print "Step by Step Decomposition:"
+		print "Taflan verður því skipt upp í minni töflur"
+		print "-------------------------------------"
 		isBCNF = False
 		while(not isBCNF):
 			isBCNF = True
 			for table in tables:
 				illDF = table.findIllDF();
 				if (illDF != 0):
-					print "For Table:", ",".join(list(table.Attrs))
-					print "the closrure of", ",".join(list(illDF.A)), "is", ",".join(list(illDF.total)), "violates BCNF."
+					print "Fyrir töfluna", "".join(list(table.Attrs)),
+					print "er lokunin fyrir", ",".join(list(illDF.A))+"->"+"".join(list(illDF.total)), "sem brýtur BCNF."
 					isBCNF = False
 					tables += table.decompose(illDF)
 					tables.remove(table)
-					print "After decomposition this table, we have:"
+					print "Eftir skiptingu fáum við nýjar töflur sem eru:"
 					for i in tables:
-						print "Table:", ",".join(list(i.Attrs))
-						print "FDs:", formFDs(i.FDs)
-						print 
+						print "Tafla", "".join(list(i.Attrs)),
+						print "með fallákveðum:", formFDs(i.FDs)
 					print "-------------------------------------"
-					break;
-
-		print "BCNF Decomposition Result:"		
-		for i in tables:
-			print "Table:", ",".join(list(i.Attrs))
-			print "FDs:", formFDs(i.FDs)
-			print 
-
-	
-
+					break
+		print "Töflurnar eru nú komnar á BCNF form!"
--- Readme.md
+++ Readme.md
@@ -1,14 +1,10 @@
 ##Overiew 
-This lib will tell you if the input is BCNF or not.
+This lib will tell you if the input is BCNF or not, print out for all combinations their closure, find all keys (and superkeys) and print out all function dependencies that have only one attributes on the right-hand-side. Forked from [Xuefeng-Zhu](https://github.com/Xuefeng-Zhu/bcnf). All prints were translated to Icelandic. 
 
 ##Instruction 
-In order to run it, just type python BCNF.py in the terminal. 
-Then follow the intruction to put the attributes of table and function dependencies  
-**Note: the input should quoted as string** 
+In order to run it, just type python BCNF.py in the terminal and input the attributes of the table and its function dependencies.
 
 *Example*:
 
 	Table Attributes:A,B,C,D,E
 	Func Dependencies:AB->C,DE->C,B->D
-
-
